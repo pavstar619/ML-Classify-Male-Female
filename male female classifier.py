434718eff2acbf021f2a6d86da7ac708fc20bf79
pavstar619 <52383535+pavstar619@users.noreply.github.com>
2020-09-16 13:27:06 +0600

    Update male female classifier.py

--- male female classifier.py
+++ male female classifier.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 #[height, weight, shoe size]
 X = [[181,80,44], [177, 70, 43], [160, 60, 38], [154, 54,37], 
-	 [166,65,40], [190,90,47], [175,64,39], [177,70,40], [159, 55,39], [171,75,42], [181,85,43]]
+	 [166,65,40], [190,90,47], [175,64,39], [177,70,40], [159, 55,39], [171,75,42], [181,85,43], [220,115,53], [210,110,51], [230,120,55]]
 
 #gender 1 is MALE, 0 is FEMALE
 Y =  [1, 0, 0, 0, 1, 1,
-	  1, 0,1, 0, 1]
+	  1, 0,1, 0, 1, 2, 2, 2]
 
 """ df = pd.read_excel('test.xlsx', 'sheet1')
 X = df['Features'].values.tolist()
@@ -38,7 +38,7 @@ predictionForAdaBoost = adaBoost.predict([[a,b,c]])
 
 
 #print all the results
-print("1 for MALE and 0 for FEMALE")
+print("2 for GIANT, 1 for MALE and 0 for FEMALE")
 print("RandomForest: ", predictionForRandomForest)
 print("SVM: ", predictionForSVM)
 print("AdaBoost: ", predictionForAdaBoost)
